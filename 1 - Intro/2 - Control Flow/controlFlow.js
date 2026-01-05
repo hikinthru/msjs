57d6250d66d659d0d5a6bc48339141e0dc835867
hikinthru <hikinthru@gmail.com>
2026-01-05 09:44:20 -0600

    Add control flow practice, clear out CodeRunner temp file

--- 1 - Intro/2 - Control Flow/controlFlow.js
+++ 1 - Intro/2 - Control Flow/controlFlow.js
@@ -29,3 +29,72 @@ console.log(!false);  // true - false is inverted
 // Combining operators
 
 let correctUsername = 'student123';
+let correctPassword = 'pass123';
+let loginSuccess = (correctUsername === 'student23') && (correctPassword === 'pass123')
+if (loginSuccess) {
+  console.log('Login successful');
+} else {
+  console.log('Login unsuccessful');
+}
+
+// Shipping Calculator
+
+let orderTotal = 51;
+let destination = 'domestic';
+let shipping = 0;
+// Calculate shipping cost
+if (destination === 'domestic') {
+  if (orderTotal < 25) {
+    shipping = 10;
+  } else if (orderTotal >= 25 && orderTotal <= 50) {
+    shipping = 5;
+  } 
+}
+
+// Format the label
+const labelWidth = 25;
+const valueWidth = 16;
+
+const left = text => String(text).padEnd(labelWidth);
+const right = text => String(text).padStart(valueWidth);
+
+// Display final costs
+console.log(`${left(`Subtotal before shipping:`)}${right(`$${orderTotal.toFixed(2)}`)}`);
+console.log(`${left(`Shipping cost:`)}${right(`$${shipping.toFixed(2)}`)}`);
+console.log(`${left(`Total cost:`)}${right(`$${(orderTotal + shipping).toFixed(2)}`)}`);
+
+console.log('');
+
+// Menu Selection with a Switch
+// let choice = 'burger';
+// switch (choice) {
+//   case 'burger':
+//     console.log("You selected a burger.");
+//     break;
+//   case 'pizza':
+//     console.log("You selected a pizza.");
+//     break;
+//   case 'salad':
+//     console.log("You selected a salad.");
+//     break;
+//   default:
+//     console.log("Invalid selection.");
+//     break;
+// }
+
+let choice = 'burger';
+switch(choice) {
+  case 'burger':
+    console.log('Burger: $10');
+    console.log('Preparation time: 15 minutes');
+    break;
+  case 'pizza':
+    console.log('Pizza: $12');
+    console.log('Preparation time: 20 minutes');
+    break;
+  case 'salad':
+    console.log('Salad: $8');
+    console.log('Preparation time: 10 minutes');
+  default:
+    console.log('Sorry, that item is not on our menu.');
+  }
